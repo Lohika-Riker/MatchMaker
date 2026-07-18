4d50a5309b604b25c15e08f653e27c3d827b2425
Henrike Lode <henrike.lode@gmail.com>
2026-07-18 19:15:26 +0200

    music manager script - increment weird factor

--- MatchMakerGame/Assets/Scripts/MusicManager.cs
+++ MatchMakerGame/Assets/Scripts/MusicManager.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using FMODUnity;
 
+public enum WeirdFactor
+    {
+        Weird0 = 0,
+        Weird1 = 1,
+        Weird2 = 2,
+        Weird3 = 3,
+        Weird4 = 4,
+        Weird5 = 5,
+        Weird6 = 6,
+        End = 7
+    }
 public class MusicManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private FMODUnity.StudioEventEmitter musicEmitter;
+    private WeirdFactor currentWeirdFactor;
+
     void Start()
     {
-        
+        currentWeirdFactor = WeirdFactor.Weird0;
+        musicEmitter.Play();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            IncreaseWeirdFactor();
+        }
     }
+
+    public void IncreaseWeirdFactor()
+    {
+        if (currentWeirdFactor < WeirdFactor.End)
+        {
+            currentWeirdFactor++;
+            SetWeirdFactor(currentWeirdFactor);
+        }
+    }
+
+    private void SetWeirdFactor(WeirdFactor weirdFactor)
+    {  
+        print($"Setting weird factor to {weirdFactor}");
+        musicEmitter.SetParameter("Weird factor", (float)weirdFactor);
+    }
+
 }
