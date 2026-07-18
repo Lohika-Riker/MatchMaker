be5426618c00a6ce1ef670fe1c948bade9ef9da3
Henrike Lode <henrike.lode@gmail.com>
2026-07-18 22:14:02 +0200

    started working on tarot game

---
+++ MatchMakerGame/Assets/Scripts/CardGame.cs
@@ -0,0 +1,47 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class CardGame : MonoBehaviour
+{
+    [SerializeField] private GameObject cardPrefab;
+    float angle = 7f;
+    float gap = 1;
+    int cardsCount = 22;
+
+    // create an array to store the cards
+    private Card [] cards;
+
+    void Start()
+    {
+        cards = new Card[cardsCount];
+        GenerateDeck();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            int cardNumber = Random.Range(0,cardsCount);
+            cards[cardNumber].RaiseCard();
+            // RaiseMiddleCard(17);
+        }
+    }
+
+    private void GenerateDeck()
+    {
+        // spawn 22 cards rotated at an increasing offset
+        float rotationOffset = cardsCount/2 * angle;
+        float positionOffset = 50;
+        for (int i = 0; i < cardsCount; i++)
+        {
+            GameObject instance = Instantiate(cardPrefab, this.transform);
+            instance.transform.DORotate(new Vector3(0,0,rotationOffset), 0f);
+            instance.transform.DOMoveX(positionOffset, 0f);
+            rotationOffset -= angle;
+            positionOffset += gap;
+
+            cards[i] = instance.GetComponent<Card>();
+        }
+    }
+}
