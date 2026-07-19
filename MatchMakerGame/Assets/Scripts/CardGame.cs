7fac371cb5ef462c99566a19ad637a00d8eab3af
Henrike Lode <henrike.lode@gmail.com>
2026-07-19 08:59:05 +0200

    center card fan on screen

--- MatchMakerGame/Assets/Scripts/CardGame.cs
+++ MatchMakerGame/Assets/Scripts/CardGame.cs
@@ -1,12 +1,11 @@
-using DG.Tweening;
 using UnityEngine;
 
 public class CardGame : MonoBehaviour
 {
     [SerializeField] private GameObject cardPrefab;
-    float angle = 7f;
-    float gap = 1;
-    int cardsCount = 22;
+    [SerializeField] private float angle = 7f;
+    [SerializeField] private float gap = 10f;
+    [SerializeField] private int cardsCount = 22;
 
     // create an array to store the cards
     private Card [] cards;
@@ -29,16 +28,19 @@ public class CardGame : MonoBehaviour
 
     private void GenerateDeck()
     {
-        // spawn 22 cards rotated at an increasing offset
-        float rotationOffset = cardsCount/2 * angle;
-        float positionOffset = 200;
         for (int i = 0; i < cardsCount; i++)
         {
-            GameObject instance = Instantiate(cardPrefab, this.transform);
-            instance.transform.DORotate(new Vector3(0,0,rotationOffset), 0f);
-            instance.transform.DOMoveX(positionOffset, 0f);
-            rotationOffset -= angle;
-            positionOffset += gap;
+            GameObject instance = Instantiate(cardPrefab, transform);
+            RectTransform cardRect = instance.GetComponent<RectTransform>();
+
+            // This ranges symmetrically around zero for both odd and even decks.
+            // For example, four cards use -1.5, -0.5, 0.5 and 1.5.
+            float offsetFromCenter = i - ((cardsCount - 1) * 0.5f);
+
+            Vector2 position = cardRect.anchoredPosition;
+            position.x = offsetFromCenter * gap;
+            cardRect.anchoredPosition = position;
+            cardRect.localRotation = Quaternion.Euler(0f, 0f, -offsetFromCenter * angle);
 
             cards[i] = instance.GetComponent<Card>();
         }
